89a2253a6e471cc1d9a121e8ab53a593ba9151e9
labradorescence <orangenewyorkcreativeagency@gmail.com>
2023-01-25 22:16:55 -0500

    prac

--- lib/file_io.py
+++ lib/file_io.py
@@ -1,8 +1,14 @@
 def write_file(file_name, file_content):
+    with open(f'{file_name}.txt', 'w', encoding='utf-8') as log_file:
+        log_file.write(file_content)
     pass
 
 def append_file(file_name, append_content):
+    with open(f'{file_name}.txt', 'a', encoding='utf-8') as log_file:
+        log_file.write(append_content)
     pass
 
 def read_file(file_name):
+    with open(f'{file_name}.txt', encoding='utf-8') as text_file:
+        return text_file.read()
     pass
